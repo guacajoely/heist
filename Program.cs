512190c3c76a2c116dd7db896815f670efd422ac
guacajoely <guacajoely@gmail.com>
2023-05-30 13:39:47 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+namespace Quest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+    
+            Heist();
+
+        }
+
+        static void Heist()
+        {
+
+            Console.WriteLine($"Heist was ran");
+
+        }
+    }
+}
